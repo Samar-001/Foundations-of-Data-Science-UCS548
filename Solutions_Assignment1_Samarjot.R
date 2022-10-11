03f64365cb422ad5a75c9ff7ba64c39c110f73ec
Samar-001 <96000974+Samar-001@users.noreply.github.com>
2022-10-11 12:35:07 +0530

    Add files via upload

---
+++ Solutions_Assignment1_Samarjot.R
@@ -0,0 +1,53 @@
+"Samarjot Singh        Assignment 1"
+
+# Q1
+A <- 23.4
+B <- 45
+C <- 678
+A
+B
+C
+
+# Q2
+ls()
+
+
+# Q3
+rm(C)
+ls()
+
+
+# Q4
+# I am learning R
+
+
+# Q5
+MyName <- 'Samarjot'
+MySurname <- 'Singh'
+cat(MyName,MySurname)
+
+
+# Q6
+D <- 0
+E <- 1
+
+
+# Q7
+o1 <- A+(B/C)
+o2 <- (C*A)-B
+
+
+# Q8
+f1 <- exp(8)
+f2 <- log(4)
+f3 <- log10(100)
+f4 <- log2(16)
+f5 <- pi
+f6 <- sqrt(729)
+
+
+# Q9
+answer1 <- (23+(4.5*2.3)/10)
+answer2 <- (456/12-log(90))
+answer3 <- (exp(5)+12/(5^6))
+answer4 <- (sqrt(45)*12/3)
